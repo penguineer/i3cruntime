4b0f061da35b1aafc154040bc9db64e9a18a1007
Christof Schulze <christof.schulze@gmx.net>
2016-01-17 20:54:26 +0100

    fix compile, introduce i3c_master.h

---
+++ i3cmaster/i3c_master.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "i3c.h"
+/*
+ ** i3c Master-related structures
+ **
+ ** Author: Christof Schulze <christof.schulze@gmx.net>
+ */
+
+uint8_t                         // Rückgabewert m. Fehlercode
+i3c_send(
+  uint8_t dst,            // I²C-Adresse
+  uint8_t opcode,         // I3C-OpCode
+  uint8_t *params,         // dazu passende Parameter
+  uint8_t par_count,      // Anzahl der Parameterpakete
+  uint8_t **response,     // Ziel-Array für die Antwort
+  uint8_t res_count       // Anzahl der Antwortpakete
+);
+
+
+uint8_t i3c_getstatus(
+  uint8_t dst,
+  struct i3c_status status
+);
+
+
+// struct frame {
+// 	// amount of packets inside frame
+// 	int length;
+// 	struct packet *packets;
+// } ;
