ccd30541dbe5fdfd59c07666bd218a2c94203a7b
Tux <tux@netz39.de>
2016-01-24 23:39:31 +0100

    New implementation for I2CEndpointException

--- i3cmaster/sys/i2c/i2cendpointexception.cpp
+++ i3cmaster/sys/i2c/i2cendpointexception.cpp
@@ -1,27 +1,30 @@
+
 #include "i2cendpointexception.h"
 
+namespace i3c {
+namespace sys {
 namespace i2c {
 
-I2CEndpointException::I2CEndpointException ( I2CAddress address, const int error, const std::string& msg )
-: m_address ( address ), m_error ( error ), m_what ( msg ) { }
+I2CEndpointException::I2CEndpointException (const I2CAddress address, const int error, const std::string& msg)
+  : m_address ( address ), m_error ( error ), m_what ( msg ) { }
 
 I2CEndpointException::~I2CEndpointException() throw() {}
 
-I2CAddress I2CEndpointException::address() const throw()
+const I2CAddress I2CEndpointException::address() const throw()
 {
-  return m_address;
+    return m_address;
 }
 
 const int I2CEndpointException::error() const throw()
 {
-  return m_error;
+    return m_error;
 }
 
 const char* I2CEndpointException::what() const throw()
 {
-  return m_what.c_str();
+    return m_what.c_str();
 }
 
-
-
-}
+} // namespace i2c
+} // namespace sys
+} // namespace i3c
--- i3cmaster/sys/i2c/i2cendpointexception.h
+++ i3cmaster/sys/i2c/i2cendpointexception.h
@@ -1,44 +1,47 @@
+#pragma once
+
+#include <stdexcept>
+#include <string>
+
 #include "i2caddress.h"
 
-#ifndef __I2CENDPOINTEXCEPTION_H
-#define  __I2CENDPOINTEXCEPTION_H
-namespace i2c 
-{
+namespace i3c {
+namespace sys {
+namespace i2c {
+
+
 //! Exception during I2C communication via an endpoint.
 class I2CEndpointException : public std::exception
 {
 public:
-  //! Create a new exception.
-  /*!
-   * @param address Device address of the I2C peer.
-   * @param errno The errno from the backing I2C library
-   * @param const msg an error message if available
-   */
-  I2CEndpointException ( I2CAddress address, const int error, const std::string& msg );
-  
-  virtual ~I2CEndpointException() throw();
-  
-  //! Get the device address corresponding to this exception.
-  virtual I2CAddress address() const throw();
-  
-  //! Get the error (errno) corresponding to this exception.
-  virtual const int error() const throw();
-  
-  //! Get the exception message.
-  /*!
-   * \returns the exception message
-   */
-  virtual const char* what() const throw();
-  
-  
-  
+        //! Create a new exception.
+        /*!
+         * @param address Device address of the I2C peer.
+         * @param errno The errno from the backing I2C library
+         * @param const msg an error message if available
+         */
+        I2CEndpointException (const I2CAddress address, const int error, const std::string& msg);
+
+        virtual ~I2CEndpointException() throw();
+
+        //! Get the device address corresponding to this exception.
+        virtual const I2CAddress address() const throw();
+
+        //! Get the error (errno) corresponding to this exception.
+        virtual const int error() const throw();
+
+        //! Get the exception message.
+        /*!
+         * \returns the exception message
+         */
+        virtual const char* what() const throw();
+
 private:
-  
-  I2CAddress m_address;
-  const int m_error ;
-  const std::string m_what;
+        const I2CAddress m_address;
+        const int m_error ;
+        const std::string m_what;
 };
 
-}
-
-#endif
+} // namespace i2c
+} // namespace sys
+} // namespace i3c
