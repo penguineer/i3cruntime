716506ab16993f907eadc4804d50d9b419de1ced
Christof Schulze <christof.schulze@gmx.net>
2016-01-31 00:05:46 +0100

    Revert "i2c_scan added"
    This reverts commit 00caacc8a32cf39b019a92029253e5b636c2985c.

--- i3cmaster/master/i3c_commhandler.cpp
+++ i3cmaster/master/i3c_commhandler.cpp
@@ -1,8 +1,7 @@
 #include "i3c_commhandler.h"
-using namespace i3c::sys::i2c;
-namespace master {
+
 namespace i3c {
-uint8_t i3c_commhandler::i3c_send ( I2CAddress dst, uint8_t opcode, uint8_t* params, uint8_t par_count, uint8_t** response, uint8_t res_count )
+uint8_t i3c_commhandler::i3c_send ( ::i2c::I2CAddress dst, uint8_t opcode, uint8_t* params, uint8_t par_count, uint8_t** response, uint8_t res_count )
 {
 
 }
@@ -13,8 +12,7 @@ void i3c_commhandler::scan_i2c_bus()
   int port, res;
 // I2CAddress::Range::max
   /* Adressbereich 7 Bit */
-  int device = open(devicename, O_RDWR);
-  for (port = (uint8_t) I2CAddress::Range::min;  port < (uint8_t)I2CAddress::Range::max; port++)
+  for (port = ::i2c::I2CAddress::Range::min;  port < i2c::I2CAddressRange::max; port++)
   {
     if (ioctl(device, I2C_SLAVE, port) < 0)
       perror("ioctl() I2C_SLAVE failed\n");
@@ -30,7 +28,7 @@ void i3c_commhandler::scan_i2c_bus()
 int i3c_commhandler::init(const char *i2cdevice) {
   int fd;
 
-//   I3CEndpointBroker epb = new I3CEndpointBroker();
+  epb = new I3CEndpointBroker();
  
 }
 
@@ -46,5 +44,4 @@ uint8_t i3c_getstatus (
   return ( 0 );
 }
 
-}
 }
