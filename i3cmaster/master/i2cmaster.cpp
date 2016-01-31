e4fea8ae5acd02b2bdf71c5dbc1d4a9d55a40c2a
Christof Schulze <christof.schulze@gmx.net>
2016-01-31 15:20:09 +0100

    add libconfig to cmake-file

--- i3cmaster/master/i2cmaster.cpp
+++ i3cmaster/master/i2cmaster.cpp
@@ -45,6 +45,12 @@ template<typename T> void print_queue ( T& q )
 }
 
 #include "../debug.h"
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+
+using namespace libconfig;
+
 
 int main()
 {
@@ -63,6 +69,122 @@ int main()
     I3CPacket p( data ,destination, ODD, ST_START );
 
     cout << p << endl;
+
+    Config cfg;
+
+    // Read the file. If there is an error, report it and exit.
+    try
+    {
+      cfg.readFile("example.cfg");
+    }
+    catch(const FileIOException &fioex)
+    {
+      std::cerr << "I/O error while reading file." << std::endl;
+      return(EXIT_FAILURE);
+    }
+    catch(const ParseException &pex)
+    {
+      std::cerr << "Parse error at " << pex.getFile() << ":" << pex.getLine()
+      << " - " << pex.getError() << std::endl;
+      return(EXIT_FAILURE);
+    }
+
+    // Get the store name.
+    try
+    {
+      string name = cfg.lookup("name");
+      cout << "Store name: " << name << endl << endl;
+    }
+    catch(const SettingNotFoundException &nfex)
+    {
+      cerr << "No 'name' setting in configuration file." << endl;
+    }
+
+    const Setting& root = cfg.getRoot();
+
+    // Output a list of all books in the inventory.
+    try
+    {
+      const Setting &books = root["inventory"]["books"];
+      int count = books.getLength();
+
+      cout << setw(30) << left << "TITLE" << "  "
+      << setw(30) << left << "AUTHOR" << "   "
+      << setw(6) << left << "PRICE" << "  "
+      << "QTY"
+      << endl;
+
+      for(int i = 0; i < count; ++i)
+      {
+	const Setting &book = books[i];
+
+	// Only output the record if all of the expected fields are present.
+	string title, author;
+	double price;
+	int qty;
+
+	if(!(book.lookupValue("title", title)
+	  && book.lookupValue("author", author)
+	  && book.lookupValue("price", price)
+	  && book.lookupValue("qty", qty)))
+	  continue;
+
+	cout << setw(30) << left << title << "  "
+	<< setw(30) << left << author << "  "
+	<< '$' << setw(6) << right << price << "  "
+	<< qty
+	<< endl;
+      }
+      cout << endl;
+    }
+    catch(const SettingNotFoundException &nfex)
+    {
+      // Ignore.
+    }
+
+    // Output a list of all books in the inventory.
+    try
+    {
+      const Setting &movies = root["inventory"]["movies"];
+      int count = movies.getLength();
+
+      cout << setw(30) << left << "TITLE" << "  "
+      << setw(10) << left << "MEDIA" << "   "
+      << setw(6) << left << "PRICE" << "  "
+      << "QTY"
+      << endl;
+
+      for(int i = 0; i < count; ++i)
+      {
+	const Setting &movie = movies[i];
+
+	// Only output the record if all of the expected fields are present.
+	string title, media;
+	double price;
+	int qty;
+
+	if(!(movie.lookupValue("title", title)
+	  && movie.lookupValue("media", media)
+	  && movie.lookupValue("price", price)
+	  && movie.lookupValue("qty", qty)))
+	  continue;
+
+	cout << setw(30) << left << title << "  "
+	<< setw(10) << left << media << "  "
+	<< '$' << setw(6) << right << price << "  "
+	<< qty
+	<< endl;
+      }
+      cout << endl;
+    }
+    catch(const SettingNotFoundException &nfex)
+    {
+      // Ignore.
+    }
+
+    return(EXIT_SUCCESS);
+
+
 //  //   printf ( "%s\n", result);
 //     std::priority_queue<int> q;
 //
